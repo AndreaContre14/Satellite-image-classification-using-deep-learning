927d05c70e5c2113d3196d8a9652a646c37d3d62
AndreaContre14 <122318938+AndreaContre14@users.noreply.github.com>
2026-01-11 18:42:01 -0500

    Add TensorFlow baseline CNN model

---
+++ src/models/baseline_cnn.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def build_baseline_cnn(input_shape, num_classes):
+    model = models.Sequential([
+        layers.Input(shape=input_shape),
+        layers.Conv2D(32, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    return model
